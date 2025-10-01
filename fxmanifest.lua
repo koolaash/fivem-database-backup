2efab4d78e60a6da7d66866b60966c9c38f7521b
Damon <73039811+koolaash@users.noreply.github.com>
2025-10-01 19:07:46 +0530

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 name 'DB BACKUP'
 author 'kool_damon'
 description 'Smart Database Baclup For [SQL]'
-repository 'https://github.com/Qbox-project/qbx_density'
+repository 'https://github.com/koolaash/fivem-database-backup'
 version '1.1'
 
 server_scripts {
